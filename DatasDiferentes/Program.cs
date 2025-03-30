33ba24914f6352bda8de4efc80a0046449a4b4c6
toffettl <toffetti0824@gmail.com>
2025-03-30 13:12:21 -0300

    Exercicios Finalizados

---
+++ DatasDiferentes/Program.cs
@@ -0,0 +1,9 @@
+﻿DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine("Data completa: " + dataAtual.ToString("dddd, dd 'de' MMMM 'de' yyyy HH:mm:ss"));
+
+Console.WriteLine("Data (dd/MM/yyyy): " + dataAtual.ToString("dd/MM/yyyy"));
+
+Console.WriteLine("Hora (HH:mm): " + dataAtual.ToString("HH:mm"));
+
+Console.WriteLine("Data com mês por extenso: " + dataAtual.ToString("dd 'de' MMMM 'de' yyyy"));
--- PlacaValica/Program.cs
+++ PlacaValica/Program.cs
@@ -1,8 +1,23 @@
-﻿Console.WriteLine("Digite a placa");
+﻿Console.Write("Digite a placa do veículo: ");
 string placa = Console.ReadLine();
 
-if (placa.Length != 7)
+if (placa.Length == 7)
 {
-    Console.WriteLine("Número de caracteres inválidos!");
+    bool primeiroCondicao = char.IsLetter(placa[0]) && char.IsLetter(placa[1]) && char.IsLetter(placa[2]);
+
+    bool segundaCondicao = char.IsDigit(placa[3]) && char.IsDigit(placa[4]) && char.IsDigit(placa[5]) && char.IsDigit(placa[6]);
+
+    if (primeiroCondicao && segundaCondicao)
+    {
+        Console.WriteLine("Placa válida!");
+    }
+    else
+    {
+        Console.WriteLine("Placa inválida!");
+    }
+}
+else
+{
+    Console.WriteLine("A placa deve conter 7 caracteres alfanuméricos");
 }
 
